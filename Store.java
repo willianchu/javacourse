123ef2b9520af965e371ba4c89b759c561055650
willianchu <willianchu@hotmail.com>
2023-01-23 13:03:41 -0800

    up date

--- Store.java
+++ Store.java
@@ -1,23 +1,20 @@
 public class Store {
-   // instance fields
-   String productType;
-   int inventoryCount;
-   double inventoryPrice;
-   
-   // constructor method
-   public Store(String product, int count, double price) {
-     productType = product;
-     inventoryCount = count;
-     inventoryPrice = price;
-   }
-   
-   // main method
-   public static void main(String[] args) {
-     Store lemonadeStand = new Store("lemonade", 42, .99);
-     Store cookieShop = new Store("cookies", 12, 3.75);
-     
-     System.out.println("Our first shop sells " + lemonadeStand.productType + " at " + lemonadeStand.inventoryPrice + " per unit.");
-     
-     System.out.println("Our second shop has " + cookieShop.inventoryCount + " units remaining.");
-   }
+  // instance fields
+  String productType;
+  
+  // constructor method
+  public Store(String product) {
+    productType = product;
+  }
+  
+  // advertise method
+  public void advertise() {
+    System.out.println("Come spend some money!");
+    System.out.println("Selling "+productType+"!");
+  }
+  
+  // main method
+  public static void main(String[] args) {
+    
+  }
 }

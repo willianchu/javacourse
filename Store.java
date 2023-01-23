7e4e7f79454948f69a126a9fafca4c79d786a12e
willianchu <willianchu@hotmail.com>
2023-01-23 13:16:35 -0800

    scope

--- Store.java
+++ Store.java
@@ -9,15 +9,15 @@ public class Store {
   
   // advertise method
   public void advertise() {
-		System.out.println("Selling " + productType + "!");
-    System.out.println("Come spend some money!");
+    String message = "Selling " + productType + "!";
+		System.out.println(message);
   }
   
   // main method
   public static void main(String[] args) {
-    Store lemonadeStand = new Store("Lemonade");
-    for(int i = 1; i <= 3; i++){
-      lemonadeStand.advertise();
-    }
+    String cookie = "Cookies";
+    Store cookieShop = new Store(cookie);
+    
+    cookieShop.advertise();
   }
 }

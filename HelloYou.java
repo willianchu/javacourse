67ba8ea9354cd2af8bd122ce1e1a755edf5fe7b7
willianchu <willianchu@hotmail.com>
2023-01-23 10:20:28 -0800

    Worked out of bounds with args when that is any

--- HelloYou.java
+++ HelloYou.java
@@ -3,7 +3,6 @@ public class HelloYou {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Hello, " + args[0]);
         System.out.println("Hello, you!");
     }
   

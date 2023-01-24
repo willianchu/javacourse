1a0ae02aebd2de968d6ac0aa16977d533b55cc05
willianchu <willianchu@hotmail.com>
2023-01-23 20:12:44 -0800

    pow for

--- Solution.java
+++ Solution.java
@@ -1,15 +1,37 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.io.*;
+import java.lang.Math;
 
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int acumulator = a;
+            for(int j=0;j<n;j++){
+                acumulator += (int) Math.pow(2,j)*b;
+                System.out.print(acumulator+" ");
+            }
+            System.out.println();
+        }
+        in.close();
+        
+    }
+}
 
 // #####################################################
 // Loops and Format output 
 
+// import java.io.*;
+// import java.math.*;
+// import java.security.*;
+// import java.text.*;
+// import java.util.*;
+// import java.util.concurrent.*;
+// import java.util.regex.*;
 // public class Solution {
 //     public static void main(String[] args) throws IOException {
 //         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in)); // read input from STDIN

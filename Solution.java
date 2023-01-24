8eb420c4de4f403896673e756658f95c6040a054
willianchu <willianchu@hotmail.com>
2023-01-23 18:55:05 -0800

    if/ remain/ and or scan

--- Solution.java
+++ Solution.java
@@ -1,15 +1,48 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+        
+        if(N%2>0 || (N<=20 && N>=6)){
+            System.out.println("Weird");
+        } else if((N<=5 && N>=2) || (N>20)) {
+            System.out.println("Not Weird");
+        }
+        
+        
     }
 }
+
+
+// ############################################
+// Scan input from STDIN. Print output to STDOUT
+// import java.util.*;
+
+// public class Solution {
+
+//     public static void main(String[] args) {
+//         Scanner scan = new Scanner(System.in);
+//         int a = scan.nextInt();
+//         int b = scan.nextInt();
+//         int c = scan.nextInt();
+
+//         System.out.println(a);
+//         System.out.println(b);
+//         System.out.println(c);
+//     }
+// }

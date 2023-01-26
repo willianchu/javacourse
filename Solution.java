e5657a602cbfee2d844d0dfa296d695cb9de46e5
willianchu <willianchu@hotmail.com>
2023-01-25 22:00:58 -0800

    end of file

--- Solution.java
+++ Solution.java
@@ -1,3 +1,19 @@
+// ################################################
+// End of File
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
+
+
 // ###############################################
 // Size of byte, short, int, long
 // import java.util.*;

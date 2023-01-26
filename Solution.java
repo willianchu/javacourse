3e1759c6f2f0ecfad859931e18ad19091939e0a1
willianchu <willianchu@hotmail.com>
2023-01-26 13:40:04 -0800

    Static init Block

--- Solution.java
+++ Solution.java
@@ -1,4 +1,43 @@
 // ################################################
+// Java Static Initializer Block
+// import java.io.*;
+// import java.util.*;
+// import java.text.*;
+// import java.math.*;
+// import java.util.regex.*;
+
+// public class Solution {
+
+// private static boolean flag; 
+// private static int B;
+// private static int H;
+
+// // static initialization block - runs before main
+// static{
+//     Scanner sc = new Scanner(System.in);
+//     B = sc.nextInt();
+//     H = sc.nextInt();
+//     sc.close();
+//     if(B>0 && H>0){
+//         flag = true;
+//     }else{
+//         System.out.println("java.lang.Exception: Breadth and height must be positive");
+//     }
+// }
+
+// public static void main(String[] args){
+// 		if(flag){
+// 			int area=B*H;
+// 			System.out.print(area);
+// 		}
+		
+// 	}//end of main
+
+// }//end of class
+
+
+
+// ################################################
 // End of File
 // import java.io.*;
 // import java.util.*;

1f2b4b8cd423005afdbc29befede609ffe2f2b5e
willianchu <willianchu@hotmail.com>
2023-01-29 12:10:18 -0800

    chartAt(pos) - 'a'  gives position

--- Solution.java
+++ Solution.java
@@ -1,3 +1,42 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    static boolean isAnagram(String a, String b) {
+        if(a.length() != b.length()) {
+            return false;
+        }
+        int[] count = new int[26];
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        
+        for(int i = 0; i < a.length(); i++) {
+            // increment the count of the character
+            count[a.charAt(i) - 'a']++;
+        }
+        for(int i = 0; i < b.length(); i++) {
+            // decrement the count of the character
+            count[b.charAt(i) - 'a']--;
+        }
+        for(int i = 0; i < 26; i++) {
+            if(count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
+
 // #######################################
 // Java String Reverse
 // .equals to string compare

c597dc482d34520a8feea4755327662fd704650e
willianchu <willianchu@hotmail.com>
2023-01-29 11:31:15 -0800

    substring end one position latersubstring end is exclusive

--- Solution.java
+++ Solution.java
@@ -1,17 +1,42 @@
+// #######################################
+// Java substring exclusive
 import java.io.*;
 import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
 
     public static void main(String[] args) {
-        
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        String B=sc.next();
-        
-        System.out.println(A.length() + B.length());
-        System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
-        System.out.println(A.substring(0, 1).toUpperCase() + A.substring(1) + " " + B.substring(0, 1).toUpperCase() + B.substring(1));
-        
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start, end));
+        // end is exclusive
     }
 }
+
+// #######################################
+// Java string manipulation
+
+// import java.io.*;
+// import java.util.*;
+
+// public class Solution {
+
+//     public static void main(String[] args) {
+        
+//         Scanner sc=new Scanner(System.in);
+//         String A=sc.next();
+//         String B=sc.next();
+        
+//         System.out.println(A.length() + B.length());
+//         System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
+//         System.out.println(A.substring(0, 1).toUpperCase() + A.substring(1) + " " + B.substring(0, 1).toUpperCase() + B.substring(1));
+        
+//     }
+// }
+
+

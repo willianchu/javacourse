b9696892d05a521e90569537a151e13bec1c00cc
willianchu <willianchu@hotmail.com>
2023-01-29 11:56:53 -0800

    == not works with string

--- Solution.java
+++ Solution.java
@@ -1,40 +1,57 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
 
-    public static String getSmallestAndLargest(String s, int k) {
-        String smallest = "";
-        String largest = "";
-        
-        int len = s.length();
-        for(int i = 0; i < len - k + 1; i++) {
-            String sub = s.substring(i, i + k);
-            if(i == 0) {
-                smallest = sub;
-                largest = sub;
-            } else {
-                if(sub.compareTo(smallest) < 0) {
-                    smallest = sub;
-                }
-                if(sub.compareTo(largest) > 0) {
-                    largest = sub;
-                }
-            }
-        }
-        
-        return smallest + "\n" + largest;
-    }
-
-
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.next();
-        int k = scan.nextInt();
-        scan.close();
-      
-        System.out.println(getSmallestAndLargest(s, k));
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String reverse = new StringBuilder(A).reverse().toString();
+        System.out.println(A.equals(reverse) ? "Yes" : "No");  
     }
 }
+
+
+// #######################################
+// Java CompareTo
+// import java.util.Scanner;
+
+// public class Solution {
+
+//     public static String getSmallestAndLargest(String s, int k) {
+//         String smallest = "";
+//         String largest = "";
+        
+//         int len = s.length();
+//         for(int i = 0; i < len - k + 1; i++) {
+//             String sub = s.substring(i, i + k);
+//             if(i == 0) {
+//                 smallest = sub;
+//                 largest = sub;
+//             } else {
+//                 if(sub.compareTo(smallest) < 0) {
+//                     smallest = sub;
+//                 }
+//                 if(sub.compareTo(largest) > 0) {
+//                     largest = sub;
+//                 }
+//             }
+//         }
+        
+//         return smallest + "\n" + largest;
+//     }
+
+
+//     public static void main(String[] args) {
+//         Scanner scan = new Scanner(System.in);
+//         String s = scan.next();
+//         int k = scan.nextInt();
+//         scan.close();
+      
+//         System.out.println(getSmallestAndLargest(s, k));
+//     }
+// }
 
 // #######################################
 // Java substring exclusive

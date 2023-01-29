6db239ee67ba149d94cbef891047424f13ece561
willianchu <willianchu@hotmail.com>
2023-01-29 11:58:24 -0800

    equals to  String function

--- Solution.java
+++ Solution.java
@@ -1,16 +1,19 @@
-import java.io.*;
-import java.util.*;
+// #######################################
+// Java String Reverse
+// .equals to string compare
+// import java.io.*;
+// import java.util.*;
 
-public class Solution {
+// public class Solution {
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
         
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        String reverse = new StringBuilder(A).reverse().toString();
-        System.out.println(A.equals(reverse) ? "Yes" : "No");  
-    }
-}
+//         Scanner sc=new Scanner(System.in);
+//         String A=sc.next();
+//         String reverse = new StringBuilder(A).reverse().toString();
+//         System.out.println(A.equals(reverse) ? "Yes" : "No");  
+//     }
+// }
 
 
 // #######################################

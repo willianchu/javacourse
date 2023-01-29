1b54144df88b4d965cdc956b488d59aff64013d0
willianchu <willianchu@hotmail.com>
2023-01-29 11:39:17 -0800

    CompareTo < negative = zero > positive

--- Solution.java
+++ Solution.java
@@ -1,22 +1,60 @@
-// #######################################
-// Java substring exclusive
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
 public class Solution {
 
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        
+        int len = s.length();
+        for(int i = 0; i < len - k + 1; i++) {
+            String sub = s.substring(i, i + k);
+            if(i == 0) {
+                smallest = sub;
+                largest = sub;
+            } else {
+                if(sub.compareTo(smallest) < 0) {
+                    smallest = sub;
+                }
+                if(sub.compareTo(largest) > 0) {
+                    largest = sub;
+                }
+            }
+        }
+        
+        return smallest + "\n" + largest;
+    }
+
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String S = in.next();
-        int start = in.nextInt();
-        int end = in.nextInt();
-        System.out.println(S.substring(start, end));
-        // end is exclusive
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
     }
 }
+
+// #######################################
+// Java substring exclusive
+// import java.io.*;
+// import java.util.*;
+// import java.text.*;
+// import java.math.*;
+// import java.util.regex.*;
+
+// public class Solution {
+
+//     public static void main(String[] args) {
+//         Scanner in = new Scanner(System.in);
+//         String S = in.next();
+//         int start = in.nextInt();
+//         int end = in.nextInt();
+//         System.out.println(S.substring(start, end));
+//         // end is exclusive
+//     }
+// }
 
 // #######################################
 // Java string manipulation

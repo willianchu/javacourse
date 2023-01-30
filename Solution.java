504ba69b409a017064e7a706c915b1641b81c34c
willianchu <willianchu@hotmail.com>
2023-01-30 12:05:59 -0800

    check valid regex function

--- Solution.java
+++ Solution.java
@@ -1,24 +1,26 @@
-import java.util.Scanner;
-import java.util.regex.*;
+// ####################################
+// Java Pattern.compile() - this is the way to check if a string is a valid regex
+// import java.util.Scanner;
+// import java.util.regex.*;
 
-public class Solution
-{
-	public static void main(String[] args){
-		Scanner in = new Scanner(System.in);
-		int testCases = Integer.parseInt(in.nextLine());
-		while(testCases>0){
-			String pattern = in.nextLine();
-          	try {
-                Pattern.compile(pattern);
-                System.out.println("Valid");
-            } catch (Exception e) {
-                System.out.println("Invalid");
-            }
+// public class Solution
+// {
+// 	public static void main(String[] args){
+// 		Scanner in = new Scanner(System.in);
+// 		int testCases = Integer.parseInt(in.nextLine());
+// 		while(testCases>0){
+// 			String pattern = in.nextLine();
+//           	try {
+//                 Pattern.compile(pattern); // compile the pattern
+//                 System.out.println("Valid");
+//             } catch (Exception e) {
+//                 System.out.println("Invalid");
+//             }
             
-            testCases--; // decrement testCases
-		}
-	}
-}
+//             testCases--; // decrement testCases
+// 		}
+// 	}
+// }
 
 
 

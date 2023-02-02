9b1ca4555a8600c18888c5148f136a4f52d5de21
willianchu <willianchu@hotmail.com>
2023-02-01 19:16:12 -0800

    final version .sort using new comparator

--- WorkBigDecimal.java
+++ WorkBigDecimal.java
@@ -26,8 +26,21 @@ class WorkBigDecimal{
     }
 
 }
+// Solution description
+// Comparator myComparator = new Comparator(){
+
+//     @Override
+//     public int compare(Object o1, Object o2) {
+//         BigDecimal a = new BigDecimal(String.valueOf(o1));
+//         BigDecimal b = new BigDecimal(String.valueOf(o2));
+//         return b.compareTo(a);
+//     }
+// };
+// Arrays.sort(s,0,n,myComparator);
 
 
+
+// older solution
 // Arrays.sort(s, 0, n, Collections.reverseOrder(new Comparator<String>() { 
 //     @Override 
 //     public int compare(String a1, String a2) {

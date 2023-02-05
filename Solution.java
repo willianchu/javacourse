7c9b84773cb4d2fca0738030910a3284954a3b59
willianchu <willianchu@hotmail.com>
2023-02-04 21:10:22 -0800

    old fashion way

--- Solution.java
+++ Solution.java
@@ -6,20 +6,33 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class IsPrime
-
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        String n = bufferedReader.readLine();
-
-        bufferedReader.close();
-
+    public static boolean isPrime(String n) {
         BigInteger bigInteger = new BigInteger(n);
-        System.out.println(bigInteger.isProbablePrime(1) ? "prime" : "not prime");
-        // Uses the Miller-Rabin primality test, which is an algorithm for testing the primality of a number.
-        // We call the function by .isProbablePrime(1) and pass the number of rounds as the parameter.
-
+        Boolean flag = true;
+        for(int i = 2; i < bigInteger.intValue(); i++) {
+            if(bigInteger.intValue() % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
     }
+
+    public static void main(String[] args) throws IOException {
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+      String n = bufferedReader.readLine();
+
+      System.out.println(isPrime(n) ? "prime" : "not prime");
+
+      bufferedReader.close();
+  }
 }
+
+        // BigInteger bigInteger = new BigInteger(n);
+        // System.out.println(bigInteger.isProbablePrime(1) ? "prime" : "not prime");
+
+        // // Uses the Miller-Rabin primality test, which is an algorithm for testing the primality of a number.
+        // // We call the function by .isProbablePrime(1) and pass the number of rounds as the parameter.

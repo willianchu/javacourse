6acc4701bde18297336d482a82356969f78d117c
willianchu <willianchu@hotmail.com>
2023-02-08 21:35:26 -0800

    Array getget

---
+++ Array2D.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Array2D {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> arrRowItems = new ArrayList<>();
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowTempItems[j]);
+                arrRowItems.add(arrItem);
+            }
+
+            arr.add(arrRowItems);
+        }
+
+        bufferedReader.close(); 
+
+        int DIMENSION = 6 - 2;
+
+        int maxSum = 0;
+
+        for (int i = 0; i < DIMENSION; i++) {
+            int currentSum = 0;
+            for (int j = 0; j < DIMENSION; j++) {
+                currentSum = arr.get(i).get(j) + arr.get(i).get(j + 1) + arr.get(i).get(j + 2) + arr.get(i + 1).get(j + 1) + arr.get(i + 2).get(j) + arr.get(i + 2).get(j + 1) + arr.get(i + 2).get(j + 2);
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                }
+            }
+          }
+        System.out.println(maxSum);
+    }
+}
+

80dcf7a09f72346483879f167e276e30dc81aa3c
willianchu <willianchu@hotmail.com>
2023-02-11 20:12:02 -0800

    catched error

--- Array2D.java
+++ Array2D.java
@@ -31,15 +31,14 @@ public class Array2D {
 
         int DIMENSION = 6 - 2;
 
-        int maxSum = 0;
+        int maxSum = 0; // initialize to 0 but if the first hourglass is negative, this will be wrong!!!
 
         for (int i = 0; i < DIMENSION; i++) {
             int currentSum = 0;
             for (int j = 0; j < DIMENSION; j++) {
                 currentSum = arr.get(i).get(j) + arr.get(i).get(j + 1) + arr.get(i).get(j + 2) + arr.get(i + 1).get(j + 1) + arr.get(i + 2).get(j) + arr.get(i + 2).get(j + 1) + arr.get(i + 2).get(j + 2);
-                if (currentSum > maxSum) {
+                if (currentSum > maxSum || i == 0 && j == 0) {
                     maxSum = currentSum;
-                }
             }
           }
         System.out.println(maxSum);

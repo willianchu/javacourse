e3d4a78b64ba01450bc12fc49eb98689287fc8da
willianchu <willianchu@hotmail.com>
2023-02-19 21:51:39 -0800

    final answer

--- SubArray.java
+++ SubArray.java
@@ -14,12 +14,15 @@ public class SubArray {
         for(int arr_i=0; arr_i < n; arr_i++){
             arr[arr_i] = in.nextInt();
         }
-
+        in.close();
+        // count the number of sub-arrays with negative sum
         int count = 0;
         for(int i = 0; i < n; i++){
             int sum = 0;
             for(int j = i; j < n; j++){
                 sum += arr[j];
+                System.out.printf("sum: %d, arr[%d]: %d", sum, j, arr[j]);
+                System.out.println();
                 if(sum < 0){
                     count++;
                 }

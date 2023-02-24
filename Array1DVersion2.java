31dfef12ffd1d59acad34853dad0b470dc3dec39
willianchu <willianchu@hotmail.com>
2023-02-23 21:25:16 -0800

    return false when return to begin

--- Array1DVersion2.java
+++ Array1DVersion2.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class Array1D {
 
     public static boolean canWin(int leap, int[] game) {
+      int i = 0;
         while (true) {
-            int i = 0;
-            if (i + leap >= game.length) {
+          if (i + leap >= game.length ) {
                 return true;
             } else if (game[i + leap] == 0) {
                 i += leap;
@@ -13,9 +13,13 @@ public class Array1D {
                 i++;
             } else if (i - 1 >= 0 && game[i - 1] == 0) {
                 i--;
+                if (i == 0) {
+                    return false;
+                }
             } else {
                 return false;
             }
+            System.out.println(i);
         }
     }
 
